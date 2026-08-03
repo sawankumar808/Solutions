cfc649572ddb92959779179d31cf1e99b3222d47
Sawan kumar <kohlisawan808@gmail.com>
2026-08-03 18:01:42 +0530

    Time: 7 ms (36.65%), Space: 19.1 MB (82.32%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,14 +1,22 @@
 class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
+    def isIsomorphic(self, s: str, t: str) -> bool:           
         freq1={}
         freq2={}
-        for ch1, ch2 in zip(s, t):
-            if ch1 in freq1 and freq1[ch1]!=ch2:
-                return False
-            if ch2 in freq2 and freq2[ch2]!=ch1:
-                return False
-            freq1[ch1]=ch2
-            freq2[ch2]=ch1
+        for i in range(len(s)):
+            fs=s[i]
+            ft=t[i]
+
+            if fs in freq1:
+                if freq1[fs]!=ft:
+                    return False
+            else:
+                freq1[fs]=ft
+            if ft in freq2:
+                if freq2[ft]!=fs:
+                    return False
+            
+            freq2[ft]=fs
+
         return True
         
             
