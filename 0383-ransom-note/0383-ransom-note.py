956a237c07334f750444135e9fe38c62d73d8bfb
Sawan kumar <kohlisawan808@gmail.com>
2026-08-11 19:06:15 +0530

    Time: 15 ms (77.76%), Space: 19.6 MB (33.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -2,10 +2,9 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         count1=Counter(ransomNote)
         count2=Counter(magazine)
-        for char in count1:
-            if count1[char]>count2[char]:
-                return False
-        return True
+        if not count1-count2:
+            return True
+        return False
 
 
 
